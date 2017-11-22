faed8c1092ff72b44c0976d30751bd99f1a4bc8f
anjara3389 <anjara3389@hotmail.com>
2017-11-22 18:43:43 -0500

    try con recursos

--- src/englishpractice/DBConnection.java
+++ src/englishpractice/DBConnection.java
@@ -9,55 +9,34 @@ import java.util.ArrayList;
 
 public class DBConnection {
 
-    private Connection connection = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
-
-    public void connectDB() throws Exception {
+    public Connection connectDB() throws Exception {
         Class.forName("com.mysql.jdbc.Driver"); // This will load the MySQL driver, each DB has its own driver
-        this.connection = DriverManager.getConnection("jdbc:mysql://localhost/englishpractice?user=root&password="); // Setup the connection with the DB
-    }
-
-    public void close() throws SQLException {
-        if (this.connection != null) {
-            this.connection.close();
-        }
-        if (this.preparedStatement != null) {
-            this.preparedStatement.close();
-        }
-        if (this.resultSet != null) {
-            this.resultSet.close();
-        }
+        return DriverManager.getConnection("jdbc:mysql://localhost/englishpractice?user=root&password="); // Setup the connection with the DB
     }
 
     public void executeUpdateQuery(String query) throws SQLException, Exception {
-        try {
-            connectDB();
-            preparedStatement = connection.prepareStatement(query);
+        try (Connection connection = connectDB(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {//Las variables que se declaran aquí sirven dentro del try. Una vez ejecutado lo de adentro del try el cierra esos recursos que se usaron
             preparedStatement.executeUpdate();
-        } finally {
-            close();
         }
     }
 
     public ArrayList<Object[]> executeSelectQuery(String query) throws SQLException, Exception {
-        connectDB();
-        preparedStatement = connection.prepareStatement(query);
+        try (Connection connection = connectDB(); PreparedStatement preparedStatement = connection.prepareStatement(query); ResultSet resultSet = preparedStatement.executeQuery()) { //Las variables que se declaran aquí sirven dentro del try. Una vez ejecutado lo de adentro del try el cierra esos recursos que se usaron
 
-        resultSet = preparedStatement.executeQuery();
-        int numCol = resultSet.getMetaData().getColumnCount();
-        ArrayList data = new ArrayList();
+            int numCol = resultSet.getMetaData().getColumnCount();
+            ArrayList data = new ArrayList();
 
-        while (resultSet.next()) {
-            Object[] obj = new Object[numCol];
-            int i = 0;
-            while (i < numCol) {
-                obj[i] = resultSet.getObject(i + 1);
-                i++;
+            while (resultSet.next()) {
+                Object[] obj = new Object[numCol];
+                int i = 0;
+                while (i < numCol) {
+                    obj[i] = resultSet.getObject(i + 1);
+                    i++;
+                }
+                data.add(obj);
             }
-            data.add(obj);
+
+            return data;
         }
-        close();
-        return data;
     }
 }
